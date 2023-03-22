a817f525bb9d5e183cd1ba2218fffd55363ca929
draco-se <73908519+draco-se@users.noreply.github.com>
2023-03-22 19:59:44 +0100

    testing skeleton loader

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: true,
   IP_API: 'https://ipapi.co/json',
   METEO_WEATHER_API: 'https://api.open-meteo.com/v1/forecast',
-  SEARCH_API: 'http://api.weatherapi.com/v1/search.json',
+  SEARCH_API: 'https://api.weatherapi.com/v1/search.json',
   SEARCH_API_KEY: '9bc27446b49f45c6936173111231203',
 };
 
