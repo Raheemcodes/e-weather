e197b0b007e48a677a2f00f706d87ecdfd9bd004
draco-se <73908519+draco-se@users.noreply.github.com>
2023-04-02 13:27:21 +0100

    developing & testing side-bar component

--- src/app/weather/side-bar/side-bar.component.spec.ts
+++ src/app/weather/side-bar/side-bar.component.spec.ts
@@ -1,10 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { SideBarComponent } from './side-bar.component';
 
 describe('SideBarComponent', () => {
   let component: SideBarComponent;
   let fixture: ComponentFixture<SideBarComponent>;
+  let de: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,10 +21,59 @@ describe('SideBarComponent', () => {
 
     fixture = TestBed.createComponent(SideBarComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('isLoading', () => {
+    it('should take 3secs to switch from true to false', fakeAsync(() => {
+      component.isLoading = true;
+
+      component.isLoading = false;
+      expect(component.isLoading).withContext('immediate').toBeTrue();
+
+      tick(3000);
+      expect(component.isLoading).withContext('after 3secs').toBeFalse();
+    }));
+
+    it('should take 3secs to switch from false to true instantly', fakeAsync(() => {
+      component.isLoading = false;
+      tick(3000);
+
+      component.isLoading = true;
+      expect(component.isLoading).toBeTrue();
+    }));
+
+    it('should contain only the skeleton of loading content if isLoading is true', () => {
+      component.isLoading = true;
+      fixture.detectChanges();
+
+      expect(de.query(By.css('li.weather-forecast.skeleton')))
+        .withContext('weather-forecast skeleton')
+        .toBeTruthy();
+
+      expect(de.query(By.css('li.weather-forecast:not(.skeleton)')))
+        .withContext('weather-forecast skeleton')
+        .toBeFalsy();
+    });
+
+    // it('should contain only the original of loaded content if isLoading is false', fakeAsync(() => {
+    //   component.hourlyData = MockFullHoulyData;
+    //   component.isLoading = false;
+    //   tick(3000);
+    //   fixture.detectChanges();
+
+    //   expect(de.query(By.css('li.weather-forecast.skeleton')))
+    //     .withContext('weather-forecast skeleton')
+    //     .toBeFalsy();
+
+    //   expect(de.query(By.css('li.weather-forecast:not(.skeleton)')))
+    //     .withContext('weather-forecast skeleton')
+    //     .toBeTruthy();
+    // }));
   });
 });
--- src/app/weather/side-bar/side-bar.component.ts
+++ src/app/weather/side-bar/side-bar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.scss']
+  styleUrls: ['./side-bar.component.scss'],
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit, OnDestroy {
+  subs: Subscription[] = [];
+  _isLoading: boolean = false;
 
+  set isLoading(val: boolean) {
+    if (val) this._isLoading = val;
+    else {
+      this.subs[0] = timer(3000).subscribe(() => {
+        this._isLoading = val;
+      });
+    }
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subs.forEach((sub) => {
+      if (sub) sub.unsubscribe();
+    });
+  }
 }
